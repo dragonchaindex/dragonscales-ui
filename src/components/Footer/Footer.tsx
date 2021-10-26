dffccf0ed63d1186edcd0b5cd230f7dac54149cb
Max Payne <uicssmaker@gmail.com>
2021-10-26 11:05:11 -0300

    fiz footer space

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -29,7 +29,7 @@ const MenuItem: React.FC<FooterProps> = ({
   ...props
 }) => {
   return (
-    <StyledFooter p={["40px 16px", null, "56px 40px 32px 40px"]} {...props} justifyContent="center">
+    <StyledFooter p={["40px 16px", null, "32px 40px 32px 40px"]} {...props} justifyContent="center">
       <Flex flexDirection="column" width={["100%", null, "1200px;"]}>
         <StyledIconMobileContainer display={["block", null, "none"]}>
           <LogoWithTextIcon isDark width="130px" />
